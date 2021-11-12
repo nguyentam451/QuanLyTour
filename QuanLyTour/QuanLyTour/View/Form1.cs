871170bc9ea735a17533956c7f4373317f8a73c3
Chung <52872325+HiamKaito@users.noreply.github.com>
2021-11-12 11:57:03 +0700

    Remaster contruction
    Cấu trúc thư mục file

--- QuanLyTour/QuanLyTour/View/Form1.cs
+++ QuanLyTour/QuanLyTour/View/Form1.cs
@@ -1,5 +1,4 @@
-﻿using BUS;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using QuanLyTour;
+using Controller;
 
 namespace QuanLyTour
 {
