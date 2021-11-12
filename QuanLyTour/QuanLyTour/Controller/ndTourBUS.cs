41cf4176d0b13157f12ff0e3c5e06d1b8c794993
Just another Dev <60642132+prohzgod@users.noreply.github.com>
2021-11-13 00:08:13 +0700

    add ndTour

---
+++ QuanLyTour/QuanLyTour/Controller/ndTourBUS.cs
@@ -0,0 +1,32 @@
+﻿using QuanLyTour.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyTour.Controller
+{
+    class ndTourBUS
+    {
+        private static ndTourBUS instance;
+
+        public static ndTourBUS Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ndTourBUS();
+                return instance;
+            }
+        }
+        private ndTourBUS() { }
+
+        public void getAll(DataGridView data)
+        {
+            data.DataSource = ndTourDAO.Instance.getAll();
+        }
+
+    }
+}
